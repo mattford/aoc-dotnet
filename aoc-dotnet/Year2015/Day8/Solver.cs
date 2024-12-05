99d7b059b428caaf7c0eaef11fa4f88fe59a0fb7
Matt Ford <mattford@users.noreply.github.com>
2024-12-05 15:59:25 +0000

    2015/8

---
+++ aoc-dotnet/Year2015/Day8/Solver.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_dotnet.Year2015.Day8;
+
+public class Solver: SolverInterface
+{
+    public string Part1(string[] input)
+    {
+        return "" + input.Select(x => x.Length - Regex.Unescape(x).Length + 2).Sum();
+    }
+
+    public string Part2(string[] input)
+    {
+        return "" + input.Select(x => Regex.Escape(x).Replace("\"", "\\\"").Length - x.Length + 2).Sum();
+    }
+}
