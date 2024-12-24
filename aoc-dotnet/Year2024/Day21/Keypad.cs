e3331b3ae35bb05650f579a01e07995b6a8f11a3
Matt Ford <mattford@users.noreply.github.com>
2024-12-24 16:30:10 +0000

    2024/21 pt1

---
+++ aoc-dotnet/Year2024/Day21/Keypad.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Immutable;
+using System.Numerics;
+
+namespace aoc_dotnet.Year2024.Day21;
+
+public class Keypad
+{
+    private char _currentKey = 'A';
+    private readonly ImmutableDictionary<char, ImmutableDictionary<char, string[]>> _buttonMap;
+    public Keypad(string[] rows)
+    {
+        var map = (
+            from y in Enumerable.Range(0, rows.Length)
+            from x in Enumerable.Range(0, rows[y].Length)
+            where rows[y][x] != ' '
+            select new KeyValuePair<Complex, char>(Complex.ImaginaryOne * y + x, rows[y][x])
+        ).ToImmutableDictionary();
+        _buttonMap = (
+            from x in map
+            select new KeyValuePair<char, ImmutableDictionary<char, string[]>>(x.Value, BuildMap(map, x.Key))
+        ).ToImmutableDictionary();
+        // foreach (var (btn, paths) in _buttonMap)
+        // {
+        //     Console.WriteLine(btn);
+        //     foreach (var path in paths)
+        //     {
+        //         Console.WriteLine($" - {path.Key} => {path.Value}");
+        //     }
+        // }
+    }
+
+    public string[] GetCommandStrings(string command)
+    {
+        var chars = command.ToCharArray();
+        var commandStrings = new List<string> { "" };
+        foreach (var commandChar in chars)
+        {
+            var nextStrings = new List<string>();
+            if (commandChar != _currentKey)
+            {
+                foreach (var ch in _buttonMap[_currentKey][commandChar])
+                {
+                    nextStrings = nextStrings.Union(commandStrings.Select(x => x + ch + "A")).ToList();
+                }
+            }
+            else
+            {
+                nextStrings = commandStrings;
+            }
+            _currentKey = commandChar;
+            commandStrings = nextStrings;
+            
+        }
+        
+        _currentKey = 'A';
+        var min = commandStrings.Min(x => x.Length);
+        return commandStrings.Distinct().Where(x => x.Length == min).ToArray();
+    }
+
+    private ImmutableDictionary<char, string[]> BuildMap(ImmutableDictionary<Complex, char> map, Complex from)
+    {
+        var paths = new Dictionary<char, string[]>();
+
+        foreach (var point in map)
+        {
+            if (point.Key == from)
+            {
+                paths.TryAdd(point.Value, [""]);
+                continue;
+            }
+            var routes = new List<string>();
+            var horizontal = point.Key.Real - from.Real;
+            var vertical = point.Key.Imaginary - from.Imaginary;
+            var hString = new string(horizontal > 0 ? '>' : '<', (int)Math.Abs(horizontal));
+            var vString = new string(vertical > 0 ? 'v' : '^', (int)Math.Abs(vertical));
+            if (map.ContainsKey(from + horizontal))
+            {
+                routes.Add(hString + vString);
+            }
+            if (map.ContainsKey(from + vertical * Complex.ImaginaryOne))
+            {
+                routes.Add(vString + hString);
+            }
+
+            paths.TryAdd(point.Value, routes.Distinct().ToArray());
+        }
+        
+        return paths.ToImmutableDictionary();
+    }
+}
---
+++ aoc-dotnet/Year2024/Day21/Solver.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_dotnet.Year2024.Day21;
+
+public class Solver: SolverInterface
+{
+    public string Part1(string[] input)
+    {
+        var numeric = "789\n456\n123\n 0A".Split("\n");
+        var directional = " ^A\n<v>".Split("\n");
+        var pipeline = new []{
+            new Keypad(numeric),
+            new Keypad(directional),
+            new Keypad(directional),
+        };
+        var total = (
+            from code in input 
+            let sequence = Translate(pipeline, code).OrderBy(x => x.Length).First()
+            let numericPart = int.Parse(Regex.Replace(code, "[^0-9]+", "")) 
+            let length = sequence.Length 
+            select length * numericPart
+        ).Sum();
+
+        return ""+total;
+    }
+
+    public string Part2(string[] input)
+    {
+        var numeric = "789\n456\n123\n 0A".Split("\n");
+        var directional = " ^A\n<v>".Split("\n");
+        var total = 0;
+        var keypads = new List<Keypad> { new(numeric) };
+        var dir = new Keypad(directional);
+        keypads.AddRange(Enumerable.Range(0, 25).Select(_ => dir));
+        var pipeline = keypads.ToArray();
+        foreach (var code in input)
+        {
+            var sequence = Translate(pipeline, code).OrderBy(x => x.Length).First();
+            var numericPart = int.Parse(Regex.Replace(code, "[^0-9]+", ""));
+            var length = sequence.Length;
+            // Console.WriteLine($"{code}: {sequence} - {length} * {numericPart} = {length * numericPart}");
+            total += length * numericPart;
+        }
+        return ""+total;
+    }
+
+    private string[] Translate(Keypad[] pipeline, string code)
+    {
+        var output = new[]{code};
+        var i = 0;
+        var cache = new Dictionary<string, string[]>();
+        foreach (var p in pipeline)
+        {
+            i++;
+            Console.WriteLine($"Processing robot {i}, keeping track of {output.Length}");
+            var next = new List<string>();
+            foreach (var c in output)
+            {
+                if (!cache.TryGetValue(c, out var cached))
+                {
+                    cached = p.GetCommandStrings(c);
+                    cache.Add(c, cached);
+                }
+                next = next.Union(cached).ToList();
+            }
+            output = next.ToArray();
+        }
+        
+        return output;
+    }
+}
