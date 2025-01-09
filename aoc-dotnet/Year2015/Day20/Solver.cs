a73c74f2329aa53255628d4a78cc6d80fa8ad116
Matt Ford <mattford@users.noreply.github.com>
2025-01-09 14:14:48 +0000

    2015/20

---
+++ aoc-dotnet/Year2015/Day20/Solver.cs
@@ -0,0 +1,35 @@
+﻿namespace aoc_dotnet.Year2015.Day20;
+
+public class Solver : SolverInterface
+{
+    public string Part1(string[] input)
+    {
+        var target = int.Parse(input[0]);
+        var houses = new int[target / 10];
+        for (var e = 1; e < target / 10; e++)
+        {
+            for (var h = e; h < target / 10; h += e)
+            {
+                houses[h - 1] += 10 * e;
+            }
+        }
+
+        return "" + (houses.ToList().FindIndex(h => h >= target) + 1);
+    }
+
+    public string Part2(string[] input)
+    {
+        var target = int.Parse(input[0]);
+        var houses = new int[target / 10];
+        for (var e = 1; e < target / 10; e++)
+        {
+            for (var h = e; h < target / 10; h += e)
+            {
+                houses[h - 1] += 11 * e;
+                if (h >= e * 50) break;
+            }
+        }
+
+        return "" + (houses.ToList().FindIndex(h => h >= target) + 1);
+    }
+}
