614d15e8ebd08db6f80a87e6772c90e57d8c022a
Matt Ford <mattford@users.noreply.github.com>
2025-01-21 21:26:38 +0000

    2017/21-22

---
+++ aoc-dotnet/Year2017/Day21/Solver.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Immutable;
+
+namespace aoc_dotnet.Year2017.Day21;
+
+public class Solver : SolverInterface
+{
+    public string Part1(string[] input)
+    {
+        var map = GetMap(input);
+        var grid = ".#.\n..#\n###".Split("\n").Select(l => l.ToCharArray().ToList()).ToList();
+        for (var t = 0; t < 5; t++)
+        {
+            grid = Enhance(grid, map);
+        }
+        return "" + grid.Sum(l => l.Count(x => x == '#'));
+    }
+
+    public string Part2(string[] input)
+    {
+        var map = GetMap(input);
+        var grid = ".#.\n..#\n###".Split("\n").Select(l => l.ToCharArray().ToList()).ToList();
+        for (var t = 0; t < 18; t++)
+        {
+            grid = Enhance(grid, map);
+        }
+        return "" + grid.Sum(l => l.Count(x => x == '#'));
+    }
+
+    private List<List<char>> Enhance(List<List<char>> grid, ImmutableDictionary<string, string> map)
+    {
+        var size = grid.Count % 2 == 0 ? 2 : 3;
+        var nextGrid = new List<List<char>>();
+        for (var y = 0; y < grid.Count; y += size)
+        {
+            var rows = grid.Skip(y).Take(size).ToArray();
+            var nextRows = new List<List<char>>();
+            for (var i = 0; i < size + 1; i++) nextRows.Add([]);
+            for (var x = 0; x < grid[y].Count; x += size)
+            {
+                var chunk = string.Join('/', rows.Select(r => new string(r.Skip(x).Take(size).ToArray())));
+                var nextChunk = map[chunk].Split('/');
+                for (var xn = 0; xn < nextChunk.Length; xn++) nextRows[xn].AddRange(nextChunk[xn]);
+            }
+            nextGrid.AddRange(nextRows);
+        }
+        return nextGrid;
+    }
+
+    private ImmutableDictionary<string, string> GetMap(string[] input)
+    {
+        var map = new Dictionary<string, string>();
+        foreach (var line in input)
+        {
+            var parts = line.Split(" => ");
+            var versions = AllVersions(parts[0]);
+            foreach (var from in versions) map.Add(from, parts[1]);
+        }
+
+        return map.ToImmutableDictionary();
+    }
+
+    private string[] AllVersions(string input)
+    {
+        var versions = new List<string> { input, FlipX(input), FlipY(input) };
+        for (var i = 0; i < 3; i++)
+        {
+            input = Rotate(input);
+            versions.AddRange([input, FlipX(input), FlipY(input)]);
+        }
+
+        return versions.Distinct().ToArray();
+    }
+
+    private string Rotate(string input)
+    {
+        // 123/456/789 => 741/852/963
+        var lines = input.Split('/');
+        var rotatedLines = new List<string>();
+        for (var i = lines[0].Length - 1; i >= 0; i--)
+        {
+            var p = lines.Select(l => l[i]).Reverse().ToArray();
+            rotatedLines.Add(new string(p));
+        }
+
+        rotatedLines.Reverse();
+        return string.Join('/', rotatedLines);
+    }
+
+    private string FlipX(string input)
+    {
+        return string.Join('/', input.Split('/').Select(r => new string(r.Reverse().ToArray())));
+    }
+
+    private string FlipY(string input)
+    {
+        return string.Join('/', input.Split('/').Reverse());
+    }
+}
---
+++ aoc-dotnet/Year2017/Day22/Solver.cs
@@ -0,0 +1,63 @@
+﻿using System.Numerics;
+
+namespace aoc_dotnet.Year2017.Day22;
+
+public class Solver: SolverInterface
+{
+    private Complex Up = -Complex.ImaginaryOne;
+    private Complex TurnRight = Complex.ImaginaryOne;
+    public string Part1(string[] input)
+    {
+        var pos = Math.Floor(input.Length / 2d) * -Up + Math.Floor(input[0].Length / 2d);
+        var dir = Up;
+        var infected = (
+            from y in Enumerable.Range(0, input.Length)
+            from x in Enumerable.Range(0, input[y].Length)
+            where input[y][x] == '#'
+            select new KeyValuePair<Complex,bool>(-Up * y + x, true)
+        ).ToDictionary();
+        const int iterations = 10000;
+        var infections = 0;
+        for (var i = 0; i < iterations; i++)
+        {
+            var isInfected = infected.GetValueOrDefault(pos, false);
+            dir *= isInfected ? TurnRight : -TurnRight;
+            if (!isInfected) infections++;
+            infected[pos] = !isInfected;
+            pos += dir;
+        }
+
+        return "" + infections;
+    }
+
+    public string Part2(string[] input)
+    {
+        var pos = Math.Floor(input.Length / 2d) * -Up + Math.Floor(input[0].Length / 2d);
+        var dir = Up;
+        var infected = (
+            from y in Enumerable.Range(0, input.Length)
+            from x in Enumerable.Range(0, input[y].Length)
+            where input[y][x] == '#'
+            select new KeyValuePair<Complex,char>(-Up * y + x, 'I')
+        ).ToDictionary();
+        const int iterations = 10000000;
+        var infections = 0;
+        for (var i = 0; i < iterations; i++)
+        {
+            var state = infected.GetValueOrDefault(pos, 'C');
+            (dir, state) = state switch
+            {
+                'C' => (dir * -TurnRight, 'W'),
+                'W' => (dir, 'I'),
+                'I' => (dir * TurnRight, 'F'),
+                'F' => (dir * TurnRight * TurnRight, 'C'),
+                _ => throw new Exception("Invalid state")
+            };
+            if (state == 'I') infections++;
+            infected[pos] = state;
+            pos += dir;
+        }
+
+        return "" + infections;
+    }
+}
