f1754c2761f28102084fba06c76d04afb4f6cc00
Matt Ford <mattford@users.noreply.github.com>
2026-01-03 13:08:19 +0000

    2025/7 2025/8

---
+++ aoc-dotnet/Year2025/Day7/Solver.cs
@@ -0,0 +1,96 @@
+﻿using System.Numerics;
+
+namespace aoc_dotnet.Year2025.Day7;
+
+public class Solver : SolverInterface
+{
+    public string Part1(string[] input)
+    {
+        var beams = new List<Complex>();
+        var splitters = new List<Complex>();
+        foreach (var y in Enumerable.Range(0, input.Length))
+        {
+            foreach (var x in Enumerable.Range(0, input[y].Length))
+            {
+                if (input[y][x] is 'S') beams.Add(Complex.ImaginaryOne * y + x);
+                if (input[y][x] is '^') splitters.Add(Complex.ImaginaryOne * y + x);
+            }
+        }
+
+        var visited = new HashSet<Complex>();
+        var splits = 0;
+        while (beams.Count > 0)
+        {
+            var nextBeams = new List<Complex>();
+            foreach (var beam in beams)
+            {
+                var current = beam;
+                while (current.Imaginary < input.Length && visited.Add(current))
+                {
+                    if (splitters.Contains(current))
+                    {
+                        nextBeams.AddRange(new List<Complex>
+                        {
+                            current - 1,
+                            current + 1
+                        }.Where(n => !visited.Contains(n)));
+                        splits++;
+                        break;
+                    }
+
+                    current += Complex.ImaginaryOne;
+                }
+            }
+
+            beams = nextBeams;
+        }
+
+        return "" + splits;
+    }
+
+    public string Part2(string[] input)
+    {
+        var beams = new List<Complex>();
+        var splitters = new List<Complex>();
+        foreach (var y in Enumerable.Range(0, input.Length))
+        {
+            foreach (var x in Enumerable.Range(0, input[y].Length))
+            {
+                if (input[y][x] is 'S') beams.Add(Complex.ImaginaryOne * y + x);
+                if (input[y][x] is '^') splitters.Add(Complex.ImaginaryOne * y + x);
+            }
+        }
+
+        return "" + QuantumTachyon(beams[0], splitters, []);
+    }
+
+    private long QuantumTachyon(Complex beam, List<Complex> splitters, Dictionary<Complex, long> cache)
+    {
+        if (cache.TryGetValue(beam, out var tachyon)) return tachyon;
+        var maxY = splitters.Max(s => s.Imaginary);
+        var routes = 0L;
+
+        var current = beam;
+        while (current.Imaginary <= maxY)
+        {
+            if (splitters.Contains(current))
+            {
+                var nexts = new List<Complex>
+                {
+                    current - 1,
+                    current + 1
+                };
+                var thisRoutes = 0L;
+                foreach (var n in nexts) thisRoutes += QuantumTachyon(n, splitters, cache);
+                cache.Add(beam, thisRoutes);
+                return thisRoutes;
+            }
+
+            current += Complex.ImaginaryOne;
+        }
+
+        routes++;
+        cache.Add(beam, routes);
+        return routes;
+    }
+}
---
+++ aoc-dotnet/Year2025/Day8/Solver.cs
@@ -0,0 +1,76 @@
+﻿namespace aoc_dotnet.Year2025.Day8;
+
+public class Solver : SolverInterface
+{
+    public string Part1(string[] input)
+    {
+        var boxes = input.Select(line => line.Split(",").Select(long.Parse).ToList()).ToList();
+        var dists = new List<(string, string, double)>();
+        while (boxes.Count > 0)
+        {
+            var first = boxes.First();
+            boxes =  boxes.Skip(1).ToList();
+            foreach (var other in boxes)
+            {
+                dists.Add((string.Join(",", first), string.Join(",", other), Dist(first, other)));
+            }
+        }
+
+        dists = dists.OrderBy(x => x.Item3).Take(1000).ToList();
+
+        var circuits = new List<List<string>>();
+        
+        foreach (var (first, other, _) in dists)
+        {
+            // Console.WriteLine($"Connecting {first} to {other}");
+            var circuit = circuits
+                .Where(x => x.Contains(first) || x.Contains(other))
+                .SelectMany(x => x)
+                .ToList();
+            circuit.AddRange([first, other]);
+            circuits = circuits.Where(x => !x.Contains(first) && !x.Contains(other)).ToList();
+            circuits.Add(circuit.Distinct().ToList());
+        }
+        return "" + circuits.OrderByDescending(c => c.Count).Take(3).Aggregate(1L, (t, c) => t * c.Count);
+    }
+
+    public string Part2(string[] input)
+    {
+        var boxes = input.Select(line => line.Split(",").Select(long.Parse).ToList()).ToList();
+        var dists = new List<(string, string, double)>();
+        while (boxes.Count > 0)
+        {
+            var first = boxes.First();
+            boxes =  boxes.Skip(1).ToList();
+            foreach (var other in boxes)
+            {
+                dists.Add((string.Join(",", first), string.Join(",", other), Dist(first, other)));
+            }
+        }
+
+        dists = dists.OrderBy(x => x.Item3).ToList();
+
+        var circuits = new List<List<string>>();
+        foreach (var (first, other, _) in dists)
+        {
+            var circuit = circuits
+                .Where(x => x.Contains(first) || x.Contains(other))
+                .SelectMany(x => x)
+                .ToList();
+            circuit.AddRange([first, other]);
+            circuits = circuits.Where(x => !x.Contains(first) && !x.Contains(other)).ToList();
+            circuits.Add(circuit.Distinct().ToList());
+            if (circuit.Distinct().Count() == input.Length)
+            {
+                return "" + long.Parse(first.Split(",")[0]) * long.Parse(other.Split(",")[0]);
+            }
+        }
+        return "Didn't find solution";
+
+    }
+
+    private double Dist(List<long> a, List<long> b)
+    {
+        return Math.Sqrt(a.Zip(b).Select(x => Math.Pow(Math.Abs(x.First - x.Second), 2)).Sum());
+    }
+}
